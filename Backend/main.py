cd8be60245b1b1a408979a0833203bbe1201527c
MakeMeFifi <chrismikolajek2006@gmail.com>
2025-06-11 15:39:52 +0200

    Erlaubt Requests aus anderen Ports

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
 
 class Item:
     def __init__(self, name: str , number: int , user : str,location : str, isBought : bool):
@@ -11,7 +15,20 @@ class Item:
 
 app = FastAPI()
 
-list = []
+origins = [
+    "http://localhost:5173",  # React dev server
+    "http://127.0.0.1:5173",  # Optional: for consistency
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # list of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],              # allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],              # allow all headers
+)
+
+list = ["Äpfel", "Birne", "Erdbeere"]
 
 
 @app.get("/")
@@ -27,9 +44,8 @@ async def items():
 async def getOneItem(id):
     return list[int(id)]
 
-@app.post("/put")
+@app.put("/put")
 async def putItem(request : Request):
     data = await request.json()
-    item = Item(data["name"], data["number"], data["user"],data["location"], False)
-    list.append(item)
+    list.append(data["name"])
     return list
