14340c34e52f5e192927345fffd685d1f793bd35
MakeMeFifi <chrismikolajek2006@gmail.com>
2025-06-16 21:57:29 +0200

    Button hinzugefügt um useStateszu testen

--- Frontend/app/(tabs)/index.jsx
+++ Frontend/app/(tabs)/index.jsx
@@ -1,12 +1,16 @@
-import { View, Text, StyleSheet, ImageBackground } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ImageBackground, Button } from 'react-native'
+import { useState } from 'react'
 import background from "@/assets/images/background.png"
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
   return (
     <View style={styles.container}>
       <ImageBackground source={background} resizeMode='cover' style={styles.image}>
         <Text style={styles.text}>Das ist ein test meine Kerle</Text>
+        <Text style={styles.text}>Hier ist ne Zahl {count}</Text>
+        <Button title='Drück mich mal' onPress={() => setCount(count + 1)} />
       </ImageBackground>
     </View>
   )
@@ -28,13 +32,16 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    padding: 10
+    padding: 10,
+    marginTop: 100
   },
   image: {
     width: '100%',
     height: '100%',
     flex: 1,
     resizeMode: 'cover',
-    justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    flexDirection: 'column'
   }
 })
